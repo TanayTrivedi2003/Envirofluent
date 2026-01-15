747e217268df688e8cb3bafc69edf7b6611e5c8e
Tanay Trivedi <135436627+TanayTrivedi2003@users.noreply.github.com>
2026-01-15 22:48:25 +0530

    fix: case-sensitive imports and cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,9 @@ import WhyChooseUs from "./components/WhyChooseUs";
 import Services from "./components/Services";
 import Testimonials from "./components/Testimonials";
 import DirectorMessage from "./components/DirectorMessage";
-import Faq from "./components/FAQ";
+import Faq from "./components/Faq";
 import InquiryForm from "./components/InqueryForm";
 import Footer from "./components/Footer";
-// import Solutions from "./components/home/Solutions";
-// import Technology from "./components/home/Technology";
-// import Workflow from "./components/home/Workflow";
-// import Projects from "./components/home/Projects";
-// import Clients from "./components/home/Clients";
-// import Testimonials from "./components/home/Testimonials";
-// import Contact from "./components/home/Contact";
-// import Footer from "./components/layout/Footer";
 
 function App() {
   return (
@@ -49,7 +41,7 @@ function App() {
       <Faq />
 
 
-     
+
 
       {/* Footer */}
       <Footer />
